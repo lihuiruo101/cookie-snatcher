fb0c3761bbe41eba17cc7e433d6daa5d7dddf29e
lihuiruo101 <yjy140206@gmail.com>
2025-06-08 13:40:26 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cookie-snatcher",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    author="지유",
+    description="자동으로 웹사이트 쿠키를 수집하는 모듈",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourname/cookie-snatcher",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.6',
+)

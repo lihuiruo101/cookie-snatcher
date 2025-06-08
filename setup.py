04173734e7790061070060c7fdb8e9f349c06900
lihuiruo101 <yjy140206@gmail.com>
2025-06-08 13:42:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=["requests"],
-    author="지유",
+    author="lsl",
     description="자동으로 웹사이트 쿠키를 수집하는 모듈",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourname/cookie-snatcher",
+    url="https://github.com/lihuiruo101/cookie-snatcher",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
